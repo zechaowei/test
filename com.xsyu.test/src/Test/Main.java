17dd8ee73fcabb49cb9b925a4da32dd41a063753
zechaowei <zechaowei2001@gmail.com>
2023-02-13 20:11:18 +0800

    Initial first

---
+++ com.xsyu.test/src/Test/Main.java
@@ -0,0 +1,16 @@
+package Test;
+
+/**
+ * ClassName: Main
+ * Package: Test
+ * Description:
+ *
+ * @Author: Mr.weizechao
+ * @Create: 2023/2/13 - 19:25
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
